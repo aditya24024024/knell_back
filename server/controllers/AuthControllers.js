47f43894d92c0e988c4a281172f04a2cead0430c
aditya24024024 <aditya24024024@gmail.com>
2025-06-09 22:15:51 +0530

    23

--- server/controllers/AuthControllers.js
+++ server/controllers/AuthControllers.js
@@ -30,6 +30,13 @@ export const signup = async (req, res, next) => {
       },
     });
 
+      res.cookie('jwt', token, {
+  httpOnly: true,
+  secure: true, // required for cross-site cookies (https)
+  sameSite: 'None', // allow cross-origin
+  maxAge: 3 * 24 * 60 * 60 * 1000 // 7 days
+});
+      
     return res.status(200).json({
     user: { id: user.id, email: user.email },
     jwt: createToken(email, user.id),
@@ -64,7 +71,7 @@ export const login = async (req, res, next) => {
 
       const token = createToken(email, user.id);
 
-      res.cookie('token', token, {
+      res.cookie('jwt', token, {
   httpOnly: true,
   secure: true, // required for cross-site cookies (https)
   sameSite: 'None', // allow cross-origin
