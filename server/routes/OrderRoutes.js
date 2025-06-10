434d86a14ce4865131d15ee27c3a84862b7d7fc6
aditya24024024 <aditya24024024@gmail.com>
2025-06-10 11:45:38 +0530

    32

--- server/routes/OrderRoutes.js
+++ server/routes/OrderRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
-import { confirmOrder, createOrder, getBuyerOrders, getSellerOrders,decline,complete } from "../controllers/OrderControllers.js";
+import { confirmOrder, createOrder, getBuyerOrders, getSellerOrders,decline,complete,getSellerRequests } from "../controllers/OrderControllers.js";
 
 export const orderRoutes = Router();
 
@@ -8,5 +8,6 @@ orderRoutes.post("/create", verifyToken, createOrder);
 orderRoutes.put("/success", verifyToken, confirmOrder);
 orderRoutes.get("/get-buyer-orders", verifyToken, getBuyerOrders);
 orderRoutes.get("/get-seller-orders", verifyToken, getSellerOrders);
+orderRoutes.get("/get-seller-requests", verifyToken, getSellerOrders);
 orderRoutes.get("/decline-order", verifyToken, decline);
 orderRoutes.put("/complete", verifyToken, complete);
