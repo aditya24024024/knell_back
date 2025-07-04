e5bb35a66973a2aeb01ccd76132117dee60e169d
aditya24024024 <aditya24024024@gmail.com>
2025-07-04 11:19:03 +0530

    addin_cloudinary

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -11,11 +11,11 @@ import {
 } from "../controllers/AuthControllers.js";
 
 import { getUserPublicProfile } from "../controllers/UserController.js";
-import { profile } from "../cloudinaryConfig.js";
+import { storage } from "../cloudinaryConfig.js";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
 
 const authRoutes = Router();
-const upload = multer({profile});
+const upload = multer({ storage });
 
 // ✅ AUTH ROUTES
 authRoutes.post("/signup", signup);
