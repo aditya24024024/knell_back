fd14b7112abf59fc6006ebd9569acf6349b0819e
aditya24024024 <aditya24024024@gmail.com>
2025-07-05 10:08:41 +0530

    Update OrderControllers.js

--- server/controllers/OrderControllers.js
+++ server/controllers/OrderControllers.js
@@ -23,7 +23,7 @@ export const createOrder = async (req, res, next) => {
             enabled: true,
           },
         });
-        const already_ordered=await prisma.orders.findUnique({
+        const already_ordered=await prisma.orders.find({
           where: { id: parseInt(gigid),
                     buyerId:req?.userId,
                     status:{
@@ -31,6 +31,25 @@ export const createOrder = async (req, res, next) => {
                     }
                  },
         });
+        // const already_ordere=await prisma.orders.find({
+        //   where: { id: parseInt(gigid),
+        //          },
+        // });
+        const already_order=await prisma.orders.find({
+          where: {id: parseInt(gigid),  
+              buyerId:req?.userId,
+                 },
+        });
+        // const already_ordered=await prisma.orders.find({
+        //   where: {
+        //             status:{
+        //                 not:"Completed"
+        //             }
+        //          },
+        // });
+            // console.log(already_ordered);
+            console.log(already_order);
+            console.log(already_ordered);
         if(already_ordered) {return res.status(401).send("You already have a pending request from the gig");}
         await prisma.orders.create({
           data: {
@@ -45,7 +64,7 @@ export const createOrder = async (req, res, next) => {
           where:{id:userId},
           select:{email:true},
         })
-        await send_mail(email);
+        // await send_mail(email);
         res.status(200).send({
           clientSecret: paymentIntent.client_secret,
           orderid: prisma.orders.id,
