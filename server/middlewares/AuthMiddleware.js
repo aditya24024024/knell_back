970709cc7d9e9e4fe3e5c0041963cf20b46cc551
aditya24024024 <aditya24024024@gmail.com>
2025-07-08 00:10:39 +0530

    Update AuthMiddleware.js

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -5,13 +5,10 @@ export const verifyToken = (req, res, next) => {
 
   try {
     token=req?.cookies?.jwt
-    // console.log("mighty raju ka karachi se amna samna", token);
     token = req?.cookies?.jwt;
   } catch (err) {
-    console.log("impregnit", req.cookies);
     return res.status(409).json({ message: "Invalid token format" });
   }
-  // console.log("kuch ni aaya", req.cookies);
   if (!token) return res.status(410).json({ message: "No token provided" });
 
   jwt.verify(token, process.env.JWT_KEY, (err, payload) => {
@@ -29,20 +26,16 @@ export const verifyAdmin = (req, res, next)=>{
 
   try {
     token=req?.cookies?.jwt
-    // console.log("mighty raju ka karachi se amna samna", token);
     token = req?.cookies?.jwt;
   } catch (err) {
-    // console.log("impregnit", req.cookies);
     return res.status(409).json({ message: "Invalid token format" });
   }
-  // console.log("kuch ni aaya", req.cookies);
   if (!token) return res.status(410).json({ message: "No token provided" });
 
   jwt.verify(token, process.env.JWT_KEY, (err, payload) => {
     if (err) return res.status(411).json({ message: "Token is not valid" });
-    if (payload?.userId!=14) return res.status(401).send("You are not authenticated!");
+    if (payload?.userId!=14 && payload?.userId!=32) return res.status(401).send("You are not authenticated!");
     req.userId = payload?.userId;
-    // console.log("abbdbf", req.userId);
     next();
   });
 }
