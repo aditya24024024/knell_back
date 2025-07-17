fae5335f69a605a5af4a17415d98bfb0bdd72d0f
aditya24024024 <aditya24024024@gmail.com>
2025-07-18 01:54:10 +0530

    Update MessagesControllers.js

--- server/controllers/MessagesControllers.js
+++ server/controllers/MessagesControllers.js
@@ -80,48 +80,48 @@ export const getMessages = async (req, res, next) => {
     return res.status(500).send("Internal Server Error");
   }
 };
-export const getMessages = async (req, res, next) => {
-  try {
-    if (req.params.orderId && req.userId) {
-      // const prisma = new PrismaClient();
-      const messages = await prisma.message.findMany({
-        where: {
-          order: {
-            id: parseInt(req.params.orderId),
-          },
-        },
-        orderBy: {
-          createdAt: "asc",
-        },
-      });
+// export const getMessages = async (req, res, next) => {
+//   try {
+//     if (req.params.orderId && req.userId) {
+//       // const prisma = new PrismaClient();
+//       const messages = await prisma.message.findMany({
+//         where: {
+//           order: {
+//             id: parseInt(req.params.orderId),
+//           },
+//         },
+//         orderBy: {
+//           createdAt: "asc",
+//         },
+//       });
 
-      await prisma.message.updateMany({
-        where: {
-          orderId: parseInt(req.params.orderId),
-          recipientId: parseInt(req.userId),
-        },
-        data: {
-          isRead: true,
-        },
-      });
-      const order = await prisma.orders.findUnique({
-        where: { id: parseInt(req.params.orderId) },
-        include: { gig: true },
-      });
-      let recipentId;
-      if (order?.buyerId === req.userId) {
-        recipentId = order.gig.userId;
-      } else if (order?.gig.userId === req.userId) {
-        recipentId = order.buyerId;
-      }
-      return res.status(200).json({ messages, recipentId });
-    }
-    return res.status(400).send("Order id is required.");
-  } catch (err) {
-    console.log(err);
-    return res.status(500).send("Internal Server Error");
-  }
-};
+//       await prisma.message.updateMany({
+//         where: {
+//           orderId: parseInt(req.params.orderId),
+//           recipientId: parseInt(req.userId),
+//         },
+//         data: {
+//           isRead: true,
+//         },
+//       });
+//       const order = await prisma.orders.findUnique({
+//         where: { id: parseInt(req.params.orderId) },
+//         include: { gig: true },
+//       });
+//       let recipentId;
+//       if (order?.buyerId === req.userId) {
+//         recipentId = order.gig.userId;
+//       } else if (order?.gig.userId === req.userId) {
+//         recipentId = order.buyerId;
+//       }
+//       return res.status(200).json({ messages, recipentId });
+//     }
+//     return res.status(400).send("Order id is required.");
+//   } catch (err) {
+//     console.log(err);
+//     return res.status(500).send("Internal Server Error");
+//   }
+// };
 
 export const getUnreadMessages = async (req, res, next) => {
   try {
