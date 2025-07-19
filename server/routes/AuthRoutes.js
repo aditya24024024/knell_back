bec5244b757fbcfdc74146e23a4d6b9c72842e2a
aditya24024024 <aditya24024024@gmail.com>
2025-07-20 00:33:18 +0530

    Update AuthRoutes.js

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -7,12 +7,13 @@ import {
   logout,
   getUserInfo,
   setUserInfo,
-  setUserImage // ✅ IMPORT this controller
+  setUserImage,
+  allUsers
 } from "../controllers/AuthControllers.js";
 
 import { getUserPublicProfile } from "../controllers/UserController.js";
 import { storage, profile } from "../cloudinaryConfig.js";
-import { verifyToken } from "../middlewares/AuthMiddleware.js";
+import { verifyToken, verifyAdmin } from "../middlewares/AuthMiddleware.js";
 
 const authRoutes = Router();
 const upload = multer({ storage:profile });
@@ -23,6 +24,7 @@ authRoutes.post("/login", login);
 authRoutes.post("/logout", verifyToken, logout);
 authRoutes.post("/get-user-info", verifyToken, getUserInfo);
 authRoutes.post("/set-user-info", verifyToken, setUserInfo);
+authRoutes.post("/all-users", verifyAdmin, allUsers);
 authRoutes.post("/set-user-image", verifyToken, upload.single("images"), setUserImage);
 
 // ✅ PUBLIC PROFILE ROUTE (GET)
