f8ac98bbdd3d30e8d14bee0c2e60118c59e9a060
aditya24024024 <aditya24024024@gmail.com>
2025-07-20 01:28:14 +0530

    Update AuthRoutes.js

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -25,8 +25,8 @@ authRoutes.post("/login", login);
 authRoutes.post("/logout", verifyToken, logout);
 authRoutes.post("/get-user-info", verifyToken, getUserInfo);
 authRoutes.post("/set-user-info", verifyToken, setUserInfo);
-authRoutes.post("/all-users", verifyAdmin, allUsers);
-authRoutes.post("/delete-users", verifyAdmin, deleteUser);
+authRoutes.get("/all-users", verifyAdmin, allUsers);
+authRoutes.get("/delete-users", verifyAdmin, deleteUser);
 authRoutes.post("/set-user-image", verifyToken, upload.single("images"), setUserImage);
 
 // ✅ PUBLIC PROFILE ROUTE (GET)
