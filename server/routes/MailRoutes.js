c295df59a6c59fe98e934240fda10435c8c98f09
aditya24024024 <aditya24024024@gmail.com>
2025-07-23 03:18:14 +0530

    Update MailRoutes.js

--- server/routes/MailRoutes.js
+++ server/routes/MailRoutes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { send_otp, verify_otp } from "../controllers/MailControllers.js";
+import { send_otp, verify_otp , forgot_send_otp } from "../controllers/MailControllers.js";
 
 export const mailRoutes = Router();
 
 mailRoutes.post("/verify-otp",verify_otp);
 mailRoutes.post("/send-otp",send_otp);
+mailRoutes.post("/send-forgot-otp",forgot_send_otp);
